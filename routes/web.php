06cf86faec42b67e04f8b164089b68c2aba50cad
shofa yasmin fauziah <yasminshofa.sy@gmail.com>
2024-02-28 20:29:03 +0700

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -53,4 +59,24 @@ Route::get('/user/{name?}', function ($name=null) {
 Route::get('/user/{name?}', function ($name="John") {
     return 'My name is '. $name;
 });
+
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+Route::get('/', [PageController::class,'index']);
+Route::get('/about', [PageController::class,'about']);
+Route::get('/articles', [PageController::class,'articles']);
+
+Route::get('/', [HomeController::class,'index']);
+Route::get('/about', [AboutController::class,'about']);
+Route::get('/articles', [ArticleController::class,'articles']);
+
+Route::resource('/photos', PhotoController::class);
+
+Route::resource('/photos', PhotoController::class)->only([
+    'index','show'
+]);
+Route::resource('/photos', PhotoController::class)->only([
+    'create','store', 'update', 'destroy'
+]);
 
